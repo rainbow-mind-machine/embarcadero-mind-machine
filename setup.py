47f6ef2de0f5d940d4e6136da8051f64fefa3dd2
Charles Reid <charlesreid1@gmail.com>
2018-05-18 01:18:18 -0700

    add packaging stuff - setup.py and a totally legit empty __init__

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from rainbowmindmachine import __version__
+
+setup(  name='rainbowmindmachine',
+        version=__version__,
+        description='The extensible framework for running Twitter bot flocks.',
+        url='https://pages.charlesreid1.com/embarcadero-mind-machine',
+        author='charlesreid1',
+        author_email='charles@charlesreid1.com',
+        test_suite='nose.collector',
+        tests_require=['nose'],
+        license='MIT',
+        packages=['embarcaderomindmachine'],
+        install_requires=['simplejson>=3.13',
+                'oauth2>=1.5',
+                'python-twitter>=3.4.1',
+                'TextBlob>=0.15',
+                'oauth2client>=3.0.0'],
+        zip_safe=False)
+
