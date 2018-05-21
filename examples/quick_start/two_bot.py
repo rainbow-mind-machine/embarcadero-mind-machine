2631f215f38f8047a7ead82c793f100d8f0f123f
Charles Reid <charlesreid1@gmail.com>
2018-05-21 00:42:57 -0700

    update two-bot example - split into parts.

--- examples/quick_start/two_bot.py
+++ examples/quick_start/two_bot.py
@@ -11,7 +11,24 @@ Where CLIENT_ID and CLIENT_SECRET are
 from your app page on Github.
 """
 
-gk = emm.GithubKeymaker()
-gk.set_apikeys_env()
-gk.make_keys_from_strings(['bot1','bot2'], keys_out_dir='/tmp/keys')
+def make_keys():
+    # Keymaker
+    gk = emm.GithubKeymaker()
+    gk.set_apikeys_env()
+    gk.make_keys_from_strings(['bot1','bot2'], keys_out_dir='keys')
 
+def run_flock():
+
+    class BlameSheep(emm.Sheep):
+        pass
+
+    # Shepherd
+    shepherd = emm.Shepherd(json_keys_dir='keys',
+                            name='blame bot flock',
+                            sheep_class=BlameSheep,
+                            filehandler = False)
+    #shepherd.perform_parallel_action('hello')
+
+if __name__=="__main__":
+    #make_keys()
+    run_flock()
